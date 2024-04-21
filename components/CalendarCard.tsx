9ead4e0c112f9dc450cb8387b9d23568af939823
Rahuletto <rahulmarban@gmail.com>
2024-04-21 16:39:06 +0530

    changed text size

--- components/CalendarCard.tsx
+++ components/CalendarCard.tsx
@@ -9,10 +9,10 @@ export default function CalendarCard({ element, month }: { element: Day, month: 
   ].join(" ");
   return (
     <>
-      <td id={new Date().getMonth() == month && new Date().getDate() == Number(element.date) ? 'today' : ""} className={classes}>{element.date}</td>
-      <td className={classes}>{element.day}</td>
-      <td className={classes}>{element.holiday}</td>
-      <td className={classes}>{element.dayOrder}</td>
+      <span id={new Date().getMonth() == month && new Date().getDate() == Number(element.date) ? 'today' : ''} className={classes}>{element.date}</span>
+      <span className={classes}>{element.day}</span>
+      <span className={classes}>{element.holiday && element.holiday?.split('/')[3] ? element.holiday?.split('-')[0] : element.holiday}</span>
+      <span className={classes}>{element.dayOrder}</span>
     </>
   );
 }
--- components/CalendarGenerator.tsx
+++ components/CalendarGenerator.tsx
@@ -4,15 +4,28 @@ import styles from "@/styles/Calendar.module.css";
 import { Calendar, Day } from "@/types/Calendar";
 import CalendarCard from "./CalendarCard";
 
-const CalendarGenerator = ({ data, page, children }: { data: Calendar, page: number, children: any }) => {
+const CalendarGenerator = ({
+  data,
+  page,
+  children,
+}: {
+  data: Calendar;
+  page: number;
+  children: any;
+}) => {
   return (
     <div className={styles.calendar}>
-      {data && data.days && data.month ? (
+      {data && data.days ? (
         <>
           {children}
           <div className={styles.calendarGrid}>
+            <span className={styles.calendarElem}>Date</span>
+            <span className={styles.calendarElem}>Day</span>
+            <span className={styles.calendarElem}>Event</span>
+            <span className={styles.calendarElem}>Day Order</span>
+
             {data.days.map((element: Day, index: any) => (
-                <CalendarCard key={index} month={page} element={element} />
+              <CalendarCard key={index} month={page} element={element} />
             ))}
           </div>
         </>
