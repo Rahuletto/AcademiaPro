88b9af7c8a2516fdbc2c6af73b4db82856ef7ef4
Rahuletto <rahulmarban@gmail.com>
2024-04-25 15:34:07 +0530

    error

--- pages/error.tsx
+++ pages/error.tsx
@@ -3,8 +3,18 @@ import { LuServerCrash } from "react-icons/lu";
 
 import styles from "@/styles/Error.module.css";
 import Header from "@/components/Header";
+import { useEffect } from "react";
+import { clearCookies } from "@/utils/cookies";
+import { useRouter } from "next/router";
 
 export default function ErrorStack({ error }: { error: Error }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    clearCookies()
+    setTimeout(() => router.reload(), 6000)
+  }, [])
+
   return (
     <>
       <Header title={"Error | Ac4D3m1APr0"} />
