77eb7fbea13758b143c742d36baf35d3a6d3017f
Rahuletto <rahulmarban@gmail.com>
2024-04-25 22:10:49 +0530

    chore: adding credit

--- pages/login.tsx
+++ pages/login.tsx
@@ -118,7 +118,7 @@ export default function Login() {
               </div>
             </form>
             <p className={styles.credits}>
-              Made by <Link href="https://marban.is-a.dev">Marban</Link>
+              Made by <Link href="https://marban.is-a.dev">Marban</Link> and <Link href="https://www.linkedin.com/in/srivishal-sivasubramanian-1a09b9240/">root-daemon</Link>
             </p>
           </div>
         </div>
