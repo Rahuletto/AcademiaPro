f50b8319236c8769a32fdffbfc4b81a7b7ac47a6
Rahuletto <rahulmarban@gmail.com>
2024-04-25 18:06:38 +0530

    uh

--- pages/timetable.tsx
+++ pages/timetable.tsx
@@ -1,21 +1,17 @@
 import { useEffect, useState } from "react";
-import Link from "next/link";
-import { useRouter } from "next/router";
 
-import type { TimeTableResponse } from "@/types/TimeTable";
-
-import { getCookie, clearCookies } from "@/utils/cookies";
+import { getCookie } from "@/utils/cookies";
 
 import Header from "@/components/Header";
 import Image from "next/image";
+import Loader from "@/components/Loader";
 
 export default function Timetable() {
-
-  const [data, setData] = useState('');
+  const [data, setData] = useState("");
 
   useEffect(() => {
     const da = localStorage.getItem("userData");
-    console.log(da)
+    console.log(da);
     const user = JSON.parse(da as string);
 
     fetch(`/api/timetable/${user.userInfo?.regNo}`, {
@@ -29,15 +25,26 @@ export default function Timetable() {
       .then((res: any) => {
         const imageUrl = URL.createObjectURL(res);
         setData(imageUrl);
-        console.log(imageUrl)
+        console.log(imageUrl);
       });
   }, []);
 
   return (
     <>
+      <Loader />
       <Header title={"Timetable | AcademiaPro"} />
 
-      <main className="root">{data && <Image className='tt-page' alt='timetable' src={data} width={2400} height={920} />}</main>
+      <main className="root">
+        {data && (
+          <Image
+            className="tt-page"
+            alt="timetable"
+            src={data}
+            width={2400}
+            height={920}
+          />
+        )}
+      </main>
     </>
   );
 }
