87ff6b0eb291b191932852be0951178d1617506b
Srivishal Sivasubramanian <47695678+root-daemon@users.noreply.github.com>
2024-04-30 14:59:19 +0530

    style(responsive): marks card for webkit

--- components/Marks.tsx
+++ components/Marks.tsx
@@ -1,20 +1,10 @@
-import MarksCard from "@/components/MarksCard";
-import type { MarksResponse } from "@/types/Marks";
+import MarksCard from '@/components/MarksCard';
+import type { MarksResponse } from '@/types/Marks';
 
 const MarksTable = ({ data }: { data: MarksResponse | null }) => {
   return (
     <>
-      <div
-        className="markGrid"
-        style={{
-          borderRadius: 34,
-          background: "var(--background)",
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fill, minmax(17rem, 1fr))",
-          gridTemplateRows: "min-content",
-          marginBottom: 32,
-        }}
-      >
+      <div className="markGrid">
         {data && data.marks ? (
           data.marks.map((element, index: any) => (
             <MarksCard
@@ -27,7 +17,7 @@ const MarksTable = ({ data }: { data: MarksResponse | null }) => {
             />
           ))
         ) : (
-          <h4 style={{ opacity: "0.6", fontSize: "16px" }}>
+          <h4 style={{ opacity: '0.6', fontSize: '16px' }}>
             No Marks have been updated. You can catch this.
           </h4>
         )}
