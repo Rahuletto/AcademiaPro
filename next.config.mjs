58b3dfd9cae36156b6ef05d2a389d8e9e28fe66c
Rahuletto <rahulmarban@gmail.com>
2024-05-01 09:44:12 +0530

    feat: fallbacks

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
-import runtimeCaching from "next-pwa/cache.js";
-import pwa from "next-pwa";
+import runtimeCaching from 'next-pwa/cache.js';
+import pwa from 'next-pwa';
 
 const withPWA = pwa({
-  dest: "public",
+  dest: 'public',
   runtimeCaching,
   register: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === 'development',
   skipWaiting: true,
+  fallbacks: {
+    image: '/fallback.png',
+    document: '/offline',
+  },
 });
 
 const config = withPWA({
@@ -16,7 +20,7 @@ const config = withPWA({
   reactStrictMode: true,
   compress: true,
   webpack: (config) => {
-    config.resolve.alias.canvas = false
+    config.resolve.alias.canvas = false;
     config.experiments = {
       topLevelAwait: true,
       layers: true,
@@ -26,20 +30,20 @@ const config = withPWA({
   async redirects() {
     return [
       {
-        source: "/betterlab",
-        destination: "https://better-lab.vercel.app",
+        source: '/betterlab',
+        destination: 'https://better-lab.vercel.app',
         permanent: true,
       },
       {
-        source: "/map",
+        source: '/map',
         destination:
-          "https://d23qowwaqkh3fj.cloudfront.net/wp-content/uploads/2022/06/srmist-ktr-campus-layout.jpg",
+          'https://d23qowwaqkh3fj.cloudfront.net/wp-content/uploads/2022/06/srmist-ktr-campus-layout.jpg',
         permanent: true,
       },
       {
-        source: "/ssr",
+        source: '/ssr',
         destination:
-          "https://academia.srmist.edu.in/#Form:Student_Service_Requests_SSR",
+          'https://academia.srmist.edu.in/#Form:Student_Service_Requests_SSR',
         permanent: true,
       },
     ];
