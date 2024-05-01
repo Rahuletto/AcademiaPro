f4be0721f9d8943c354f63a1f46c51b2bd8ba214
Rahuletto <rahulmarban@gmail.com>
2024-05-01 11:33:09 +0530

    chore: sw

---
+++ public/fallback-5Fgusch4RNuy0x87Bjpk5.js
@@ -0,0 +1,13 @@
+(() => {
+  'use strict';
+  self.fallback = async (e) => {
+    switch (e.destination) {
+      case 'document':
+        return caches.match('/offline', { ignoreSearch: !0 });
+      case 'image':
+        return caches.match('/fallback.png', { ignoreSearch: !0 });
+      default:
+        return Response.error();
+    }
+  };
+})();
