2041040079a6ee7dc5befcffddc4de7d943e340f
Rahuletto <rahulmarban@gmail.com>
2024-05-01 11:51:16 +0530

    fix: dont clear cookies at error

--- pages/error.tsx
+++ pages/error.tsx
@@ -1,31 +1,30 @@
-import { FaDoorOpen } from "react-icons/fa6";
-import { LuServerCrash } from "react-icons/lu";
+import { FaDoorOpen } from 'react-icons/fa6';
+import { LuServerCrash } from 'react-icons/lu';
 
-import styles from "@/styles/Error.module.css";
-import Header from "@/components/Header";
-import { useEffect } from "react";
-import { clearCookies } from "@/utils/cookies";
-import { useRouter } from "next/router";
+import styles from '@/styles/Error.module.css';
+import Header from '@/components/Header';
+import { useEffect } from 'react';
+import { clearCookies } from '@/utils/cookies';
+import { useRouter } from 'next/router';
 
 export default function ErrorStack({ error }: { error: Error }) {
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
-    clearCookies()
-    setTimeout(() => router.push('/login'), 4000)
-  }, [])
+    setTimeout(() => router.back(), 5000);
+  }, []);
 
   return (
     <>
-      <Header title={"Error | Ac4D3m1APr0"} />
+      <Header title={'Error | Ac4D3m1APr0'} />
 
       <main className={styles.main}>
         <div className={styles.container}>
           <h1
             style={
               error
-                ? { fontSize: "98px", color: "var(--red)" }
-                : { fontSize: "98px", color: "var(--green)" }
+                ? { fontSize: '98px', color: 'var(--red)' }
+                : { fontSize: '98px', color: 'var(--green)' }
             }
           >
             {error ? <LuServerCrash /> : <FaDoorOpen />}
@@ -39,7 +38,7 @@ export default function ErrorStack({ error }: { error: Error }) {
           <pre className={styles.errorPre}>
             <code className={styles.errorStack}>
               {error?.stack ||
-                "Wait, there is no error stack here. wtf\n\nGo back to home, there is nothing here... is there?"}
+                'Wait, there is no error stack here. wtf\n\nGo back to home, there is nothing here... is there?'}
             </code>
           </pre>
         </div>
