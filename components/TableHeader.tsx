ddea7429fbae28e471c19913f3ad14f1707cf324
Rahul Marban <rahulmarban@gmail.com>
2024-05-02 14:12:50 +0530

    Update TableHeader.tsx

--- components/TableHeader.tsx
+++ components/TableHeader.tsx
@@ -6,6 +6,15 @@ export function TableHeader() {
   const [d, setD] = useState(new Date(
     new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }),
   ))
+
+  const intervalRef = useInterval(
+    () => {
+      setD(new Date(
+    new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }),
+  ))
+    },
+    60 * 1000
+  );
   
   const arr = [];
   for (let i = 0; i < startingTimesSlot.length; i++) {
@@ -27,4 +36,23 @@ export function TableHeader() {
     );
   }
   return <tr className="bg-light-gray">{...arr}</tr>;
+}
+
+function useInterval(callback: any, delay: number | null) {
+  const intRef: any = useRef();
+  const cb = useRef(callback);
+
+  useEffect(() => {
+    cb.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (typeof delay === "number") {
+      intRef.current = window.setInterval(() => cb.current(), delay);
+
+      return () => window.clearInterval(intRef.current);
+    }
+  }, [delay]);
+
+  return intRef;
 }
