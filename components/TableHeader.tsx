c926849b62ce026d3bc58c078e28a23be87b27e4
Rahul Marban <rahulmarban@gmail.com>
2024-05-02 14:13:26 +0530

    Update TableHeader.tsx

--- components/TableHeader.tsx
+++ components/TableHeader.tsx
@@ -1,38 +1,35 @@
-import { endingTimesSlot, startingTimesSlot } from '@/types/Times';
-import { timeRange } from '@/utils/range';
-import { useState } from 'react'
+import { endingTimesSlot, startingTimesSlot } from "@/types/Times";
+import { timeRange } from "@/utils/range";
+import { useState } from "react";
 
 export function TableHeader() {
-  const [d, setD] = useState(new Date(
-    new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }),
-  ))
-
-  const intervalRef = useInterval(
-    () => {
-      setD(new Date(
-    new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }),
-  ))
-    },
-    60 * 1000
+  const [d, setD] = useState(
+    new Date(new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" }))
   );
-  
+
+  const intervalRef = useInterval(() => {
+    setD(
+      new Date(new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" }))
+    );
+  }, 60 * 1000);
+
   const arr = [];
   for (let i = 0; i < startingTimesSlot.length; i++) {
     arr.push(
       <th
-        title={startingTimesSlot[i] + '-' + endingTimesSlot[i]}
+        title={startingTimesSlot[i] + "-" + endingTimesSlot[i]}
         className="head-time"
       >
         <span
           className={
-            timeRange(d, startingTimesSlot[i] + '-' + endingTimesSlot[i])
-              ? 'current-time'
-              : ''
+            timeRange(d, startingTimesSlot[i] + "-" + endingTimesSlot[i])
+              ? "current-time"
+              : ""
           }
         >
           {i + 1}
         </span>
-      </th>,
+      </th>
     );
   }
   return <tr className="bg-light-gray">{...arr}</tr>;
