a2135e7397bc41a44fc59534da5aa0d57edd3944
Rahul Marban <rahulmarban@gmail.com>
2024-05-02 12:47:57 +0530

    Update range.ts

--- utils/range.ts
+++ utils/range.ts
@@ -1,11 +1,8 @@
-export function timeRange(timeRange: string) {
+export function timeRange(now: Date, timeRange: string) {
   const [startTime, endTime] = timeRange.split('-');
   const [startHour, startMinute] = startTime.split(':').map(Number);
   const [endHour, endMinute] = endTime.split(':').map(Number);
 
-  const now = new Date(
-    new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }),
-  );
   const currentHour = now.getHours();
   const currentMinute = now.getMinutes();
 
