32752ad971ed7632c7a2b1d688706fb4c15106c2
Rahuletto <rahulmarban@gmail.com>
2024-05-03 19:57:57 +0530

    fix: login is broken

--- providers/DayProvider.tsx
+++ providers/DayProvider.tsx
@@ -14,27 +14,30 @@ export function DayProvider({ children }: any) {
   const [day, setDay] = useState<DayOrderResponse | null>(null);
 
   useEffect(() => {
-    fetch(`${URL}/api/dayorder`, {
-      cache: 'default',
-      method: 'GET',
-      headers: {
-        'X-CSRF-Token': getCookie('token') as string,
-        'Set-Cookie': getCookie('token') as string,
-        Cookie: getCookie('token') as string,
-        Connection: 'keep-alive',
-        'Accept-Encoding': 'gzip, deflate, br, zstd',
-        'Cache-Control': 'public, s-maxage=86400, stale-while-revalidate=7200',
-      },
-    })
-      .then((r) => r.json())
-      .then((res) => {
-        if (res.token_refresh) {
-          clearCookies();
-          window.location.reload();
-        } else {
-          setDay(res);
-        }
-      });
+    const cookie = getCookie('token');
+    if (cookie)
+      fetch(`${URL}/api/dayorder`, {
+        cache: 'default',
+        method: 'GET',
+        headers: {
+          'X-CSRF-Token': cookie,
+          'Set-Cookie': cookie,
+          Cookie: cookie,
+          Connection: 'keep-alive',
+          'Accept-Encoding': 'gzip, deflate, br, zstd',
+          'Cache-Control':
+            'public, s-maxage=86400, stale-while-revalidate=7200',
+        },
+      })
+        .then((r) => r.json())
+        .then((res) => {
+          if (res.token_refresh) {
+            clearCookies();
+            window.location.reload();
+          } else {
+            setDay(res);
+          }
+        });
   }, []);
 
   return <DayContext.Provider value={day}>{children}</DayContext.Provider>;
