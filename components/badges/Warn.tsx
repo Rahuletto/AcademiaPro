bd9227be5c76d2dadf24e10f85ff6ec04325c272
Rahuletto <rahulmarban@gmail.com>
2024-06-25 14:39:26 +0530

    feat: tailwind config

--- components/badges/Warn.tsx
+++ components/badges/Warn.tsx
@@ -1,8 +1,11 @@
 export default function Warn() {
   return (
-    <div className="warn-toast">
-      <h3>Warning</h3>
-      <p>
+    <div
+      style={{ backgroundColor: 'rgb(23, 21, 15)' }}
+      className="flex flex-col gap-2 absolute right-4 bottom-4 rounded-3xl px-5 py-3 border-yellow border-2"
+    >
+      <h3 className="text-yellow text-xl">Warning</h3>
+      <p className="opacity-60 text-sm max-w-[350px]">
         SRM Academia didn{"'"}t provide necessary data. Its probably because of
         a new academic year. If its flagged wrong, please logout and try again.
       </p>
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        backgroundDark: 'var(--background-dark)',
+        backgroundDarker: 'var(--background-darker)',
+        backgroundLight: 'var(--background-light)',
+        buttons: 'var(--buttons)',
+        sideActive: 'var(--side-active)',
+        accent: 'var(--accent)',
+        theory: 'var(--theory)',
+        yellow: 'var(--yellow)',
+        practical: 'var(--practical)',
+        green: 'var(--green)',
+        red: 'var(--red)',
+        color: 'var(--color)',
+      },
+    },
+  },
+  plugins: [],
+};
