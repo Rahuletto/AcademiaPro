9756ae2188535c6465c7f8d0d3875ccc9ffa7fe4
Srivishal Sivasubramanian <47695678+root-daemon@users.noreply.github.com>
2024-06-25 22:43:43 +0530

    Update Marks.tsx

--- components/Marks.tsx
+++ components/Marks.tsx
@@ -19,7 +19,7 @@ const MarksTable = ({ data }: { data: MarksResponse | null }) => {
         </div>
       ) : (
         <div className="m-[12px_24px] flex h-[280px] items-center justify-center rounded-[22px] bg-backgroundLight">
-          <h4 className="text-center text-[16px] opacity-80">
+          <h4 className="text-center text-base opacity-80">
             No Marks have been updated. You can catch this.
           </h4>
         </div>
