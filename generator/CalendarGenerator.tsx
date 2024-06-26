51b44424905961996318365e0a50ec5cd03b2064
Rahuletto <rahulmarban@gmail.com>
2024-06-26 23:08:07 +0530

    feat: redid calendar to be smooth and better on mobile

--- generator/CalendarGenerator.tsx
+++ generator/CalendarGenerator.tsx
@@ -3,7 +3,7 @@ import "react-loading-skeleton/dist/skeleton.css";
 import styles from "@/styles/Calendar.module.css";
 import { Calendar, Day } from "@/types/Calendar";
 import CalendarCard from "../components/cards/CalendarCard";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 
 const CalendarGenerator = ({
   data,
@@ -14,21 +14,36 @@ const CalendarGenerator = ({
   page: number;
   children: ReactNode;
 }) => {
+  const header = useRef<HTMLDivElement>(null);
+  const body = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const headerDiv = header.current;
+    const bodyDiv = body.current;
+
+    bodyDiv?.addEventListener("scroll", () => {
+      headerDiv?.scrollTo(bodyDiv.scrollLeft, 0);
+    });
+
+    
+  }, [])
   return (
-    <div className={styles.calendar}>
+    <div className={`${styles.calendar} overflow-hidden p-1`}>
       {data && data.days ? (
         <>
           {children}
-          <div className={styles.calendarGrid}>
-            <span className={styles.calendarElem}>Date</span>
-            <span className={styles.calendarElem}>Day</span>
-            <span className={styles.calendarElem}>Event</span>
-            <span className={styles.calendarElem}>Day Order</span>
 
-            {data.days.map((element: Day, index: number) => (
-              <CalendarCard key={index} month={page} element={element} />
-            ))}
-          </div>
+            <div ref={header} className={`${styles.calendarHead} overflow-none w-auto`}>
+              <span className={styles.calendarElem}>Date</span>
+              <span className={styles.calendarElem}>Day</span>
+              <span className={styles.calendarElem}>Event</span>
+              <span className={styles.calendarElem}>Day Order</span>
+            </div>
+            <div ref={body} className={`${styles.calendarGrid} w-auto overflow-auto`}>
+              {data.days.map((element: Day, index: number) => (
+                <CalendarCard key={index} month={page} element={element} />
+              ))}
+            </div>
         </>
       ) : (
         <>
