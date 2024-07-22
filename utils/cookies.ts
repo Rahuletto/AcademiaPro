c4600c5fe354f6315c51846fc30bd2656b3ff6dd
Rahuletto <rahulmarban@gmail.com>
2024-07-22 06:49:05 +0530

    a

--- utils/cookies.ts
+++ utils/cookies.ts
@@ -1,11 +1,19 @@
-export function setCookie(c_name: string, value: string, exdays?: number | null): void {
+export function setCookie(
+  c_name: string,
+  value: string,
+  exdays?: number | null,
+) {
   const exdate = new Date();
- exdate.setDate(exdate.getDate() + (exdays || 31 * 24 * 60 * 60 * 1000));
-const c_value = escape(value) + (exdays === null ? "" : "; expires=" + exdate.toUTCString());
- document.cookie = c_name + "=" + c_value + ";secure";
+  exdate.setDate(exdate.getDate() + (exdays || 20 * 60 * 60 * 1000));
+  const c_value =
+    escape(value) +
+    (exdays === null ? "" : "; expires=" + exdate.toUTCString());
+  document.cookie = c_name + "=" + c_value + ";secure";
 }
 
 export function getCookie(c_name: string) {
+  if (localStorage.getItem(c_name)) return localStorage.getItem(c_name);
+
   let i;
   let x;
   let y;
@@ -21,12 +29,14 @@ export function getCookie(c_name: string) {
 }
 
 export function clearCookies() {
+  const cookies = document.cookie.split(";");
   localStorage.clear();
   sessionStorage.clear();
 
-  document.cookie.split(";").forEach(function (c) {
-    document.cookie = c
-      .replace(/^ +/, "")
-      .replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
-  });
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i];
+    const eqPos = cookie.indexOf("=");
+    const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  }
 }
