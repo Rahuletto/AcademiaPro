589afa80b4cd4c8c36c0a3738a910ce10b0f70de
Rahuletto <rahulmarban@gmail.com>
2024-07-26 20:24:18 +0530

    style: new design of sidebar

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,34 +1,68 @@
-import { useState } from "react";
+import { useState, ReactNode, useEffect, useRef } from "react";
 import Link from "./SidebarLink";
-import { FaBookOpen, FaGraduationCap, FaLink } from "react-icons/fa6";
+import {
+  FaAnglesLeft,
+  FaAnglesRight,
+  FaBookOpen,
+  FaGraduationCap,
+  FaLink,
+} from "react-icons/fa6";
 import { HiLightningBolt } from "react-icons/hi";
 import { BsCalendar2WeekFill } from "react-icons/bs";
 import DayOrder from "../badges/Day";
 import Hours from "../badges/Hours";
-import { ReactNode } from "react";
-import { TbMenu2 } from "react-icons/tb";
 
 export function Sidebar({ children }: { children: ReactNode }) {
-  const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+  const content = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState(true);
+
+  useEffect(() => {
+    if (ref.current && !isOpen) {
+      const handleMouseEnter = () => {
+        setIsOpen(true);
+      };
+      const handleMouseLeave = () => {
+        setIsOpen(false);
+      };
+
+      if (content.current) {
+        content.current.addEventListener("mouseenter", handleMouseLeave);
+      }
+
+      ref.current.addEventListener("mouseenter", handleMouseEnter);
+
+      return () => {
+        ref.current?.removeEventListener("mouseenter", handleMouseEnter);
+      };
+    }
+  }, [isOpen]);
 
   return (
     <>
       <div className="fixed left-0 top-0 flex h-screen w-screen flex-row justify-between gap-10 p-3 transition duration-300">
         <div
-          className={`fixed left-0 top-0 h-full transform bg-light-background-light p-4 text-white transition-transform dark:bg-dark-background-normal ${
-            isOpen ? "translate-x-0" : "-translate-x-full"
-          } w-[300px]`}
+          ref={ref}
+          className={`fixed left-0 top-0 flex h-full transform flex-col gap-2 bg-light-background-light p-4 text-white transition-transform duration-300 dark:bg-dark-background-normal ${
+            isOpen ? "translate-x-0" : "-translate-x-96 lg:-translate-x-56"
+          } w-[310px] p-8`}
         >
-          <div className="text-color flex items-center justify-between">
-            <h1 className="text-3xl font-semibold">AcademiaPro</h1>
-          </div>
-          <div className="my-4 flex gap-2">
-            <DayOrder />
-            <Hours />
-          </div>
-          <hr className="border-t-light-side dark:border-t-dark-side" />
+          {isOpen && (
+            <>
+              <div>
+                <div className="text-color flex items-center justify-between">
+                  <h1 className="text-3xl font-semibold">AcademiaPro</h1>
+                </div>
+                <div className="my-4 flex gap-2">
+                  <DayOrder />
+                  <Hours />
+                </div>
+              </div>
+              <hr className="border-t-light-side dark:border-t-dark-side" />
+            </>
+          )}
 
-          <div className="text-md flex flex-col gap-1 font-semibold">
+          <div className="text-md flex flex-col gap-2 font-semibold">
             <Link href="/academia">
               <FaBookOpen className="text-xl" />
               Home
@@ -61,23 +95,42 @@ export function Sidebar({ children }: { children: ReactNode }) {
             <HiLightningBolt className="text-xl" />
             eLab
           </Link>
+
+          <button
+            type="button"
+            name="Open navbar"
+            className={`fixed bottom-5 hidden rounded-full p-3 lg:block ${isOpen ? "right-5 bg-light-error-background dark:bg-dark-error-background" : "right-7 bg-transparent"}`}
+            onClick={() => setIsOpen((prev) => !prev)}
+          >
+            {isOpen ? (
+              <FaAnglesLeft className="text-xl dark:text-dark-error-color" />
+            ) : (
+              <FaAnglesRight className="text-xl" />
+            )}
+          </button>
         </div>
 
         <div
-          className={`h-full flex-1 transform transition-transform ${isOpen ? "ml-[300px]" : "ml-0"}`}
+          className={`h-full flex-1 transform duration-300 ${isOpen ? "ml-[300px]" : "ml-0 lg:ml-[55px]"}`}
         >
-          <div className="m-0.5 h-full w-full animate-fadeIn overflow-y-auto overflow-x-hidden rounded-2xl bg-light-background-dark p-4 md:p-7 dark:bg-dark-background-dark">
+          <div
+            ref={content}
+            className="m-0.5 h-full w-full animate-fadeIn overflow-y-auto overflow-x-hidden rounded-2xl bg-light-background-dark p-4 md:p-7 dark:bg-dark-background-dark"
+          >
             {children}
           </div>
         </div>
-
         <button
           type="button"
           name="Open navbar"
-          className="fixed bottom-5 right-5 rounded-full bg-gray-800 p-3 text-white shadow-lg"
-          onClick={() => setIsOpen(!isOpen)}
+          className={`fixed bottom-5 rounded-full p-3 lg:hidden ${isOpen ? "right-5 bg-light-error-background dark:bg-dark-error-background" : "right-5 bg-transparent"}`}
+          onClick={() => setIsOpen((prev) => !prev)}
         >
-          <TbMenu2 className="text-xl" />
+          {isOpen ? (
+            <FaAnglesLeft className="text-xl dark:text-dark-error-color" />
+          ) : (
+            <FaAnglesRight className="text-xl" />
+          )}
         </button>
       </div>
     </>
