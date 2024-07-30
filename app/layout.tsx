05dbf29ebbac59fc0d518620819490d09df2f6b4
Rahuletto <rahulmarban@gmail.com>
2024-07-30 20:44:31 +0530

    fix: fixing the pwa moment

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,14 @@ const APP_DEFAULT_TITLE = "AcademiaPro";
 const APP_TITLE_TEMPLATE = "%s - PWA App";
 const APP_DESCRIPTION =
   "📖 University data, beautifully presented at your fingertips";
+const PRODUCTION_URL = "https://academia-pro.vercel.app";
 
 export const metadata: Metadata = {
+  metadataBase: new URL(
+    process.env.NODE_ENV === "production"
+      ? PRODUCTION_URL
+      : "http://localhost:0243",
+  ),
   applicationName: APP_NAME,
   title: {
     default: APP_DEFAULT_TITLE,
@@ -33,7 +39,6 @@ export const metadata: Metadata = {
     capable: true,
     statusBarStyle: "default",
     title: APP_DEFAULT_TITLE,
-    // startUpImage: [],
   },
   formatDetection: {
     telephone: false,
@@ -46,14 +51,39 @@ export const metadata: Metadata = {
       template: APP_TITLE_TEMPLATE,
     },
     description: APP_DESCRIPTION,
+    images: [
+      {
+        url: "/images/og.png",
+        width: 1280,
+        height: 720,
+        alt: APP_NAME,
+      },
+    ],
   },
   twitter: {
-    card: "summary",
+    card: "summary_large_image",
     title: {
       default: APP_DEFAULT_TITLE,
       template: APP_TITLE_TEMPLATE,
     },
     description: APP_DESCRIPTION,
+    images: ["/images/og.png"],
+  },
+  icons: {
+    icon: [
+      {
+        url: "/icons/android-icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      {
+        url: "/icons/maskable_icon_x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+    ],
   },
 };
 
