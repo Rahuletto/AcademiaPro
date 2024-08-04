3b2da5084643d87c5be221a4b62c61d73df2b8d7
Srivishal Sivasubramanian <srivishal.sivasubramanian@gmail.com>
2024-08-04 23:51:35 +0530

    fix: footer style

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,11 +6,11 @@ export default function Footer({ className = "" }) {
       className={`flex items-center justify-center text-sm opacity-60 ${className}`}
     >
       Made by{" "}
-      <div className="px-2 underline hover:cursor-pointer">
+      <div className="px-2 font-semibold underline hover:cursor-pointer">
         <a href="https://marban.is-a.dev/">Marban</a>
       </div>{" "}
       &{" "}
-      <div className="px-2 underline hover:cursor-pointer">
+      <div className="px-2 font-semibold underline hover:cursor-pointer">
         <a href="https://www.linkedin.com/in/srivishal-sivasubramanian-1a09b9240/">
           root-daemon
         </a>
