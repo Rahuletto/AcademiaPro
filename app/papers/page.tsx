e904eeb2963856bd771f22efa0ae738892d5c6b5
Srivishal Sivasubramanian <47695678+root-daemon@users.noreply.github.com>
2024-08-11 22:08:08 +0530

    feat: SOS

--- app/papers/page.tsx
+++ app/papers/page.tsx
@@ -1,14 +1,32 @@
+"use client";
 import { Sidebar } from "@/components/Sidebar";
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import { colors } from "@/utils/ProfileColor";
 
 export default function Papers() {
+  useEffect(() => {
+    const audio = new Audio("/audio/secret.mp3");
+    audio.muted = true;
+    audio.loop = true;
+    audio
+      .play()
+      .then(() => {
+        audio.muted = false;
+      })
+      .catch((error) => {});
+
+    return () => {
+      audio.pause();
+      audio.currentTime = 0;
+    };
+  }, []);
+
   return (
     <Sidebar>
       <div className="flex h-[90vh] w-full flex-col items-center justify-center gap-12">
         <Image src="/anim.gif" alt="papers" width={700} height={700} />
-        <p className="mx-3 font-sans text-sm font-medium text-light-color dark:opacity-60 dark:text-dark-color">
+        <p className="mx-3 font-sans text-sm font-medium text-light-color dark:text-dark-color dark:opacity-60">
           Thank you <span style={{ color: colors[0] }}>Darshan 🥚</span>,{" "}
           <span style={{ color: colors[1] }}>Harshan</span>,{" "}
           <span style={{ color: colors[2] }}>Pranav</span>,{" "}
