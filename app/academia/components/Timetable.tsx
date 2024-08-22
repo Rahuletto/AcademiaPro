80e2a2912936c40073cb5fb4514376a3e5ae287c
Rahuletto <rahulmarban@gmail.com>
2024-08-22 06:55:35 +0530

    feat: refresh timetable too

--- app/academia/components/Timetable.tsx
+++ app/academia/components/Timetable.tsx
@@ -7,6 +7,7 @@ import { useEffect, useRef, useState } from "react";
 import { FiChevronLeft, FiChevronRight, FiInfo } from "react-icons/fi";
 
 import dynamic from "next/dynamic";
+import Refresh from "@/components/Refresh";
 
 const InfoPopup = dynamic(
   () => import("./subcomponents/Attendance/InfoPopup").then((a) => a.default),
@@ -66,32 +67,36 @@ export default function Timetable() {
 
   return (
     <section id="timetable" className="flex flex-col gap-6">
-      <div className="flex items-center gap-4">
-        <h1 className="text-2xl font-semibold">Timetable</h1>
-        <Link
-          href="/timetable"
-          secondary
-          className="flex items-center justify-center text-sm text-light-accent dark:text-dark-accent"
-        >
-          Download
-        </Link>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <h1 className="text-2xl font-semibold">Timetable</h1>
+          <Link
+            href="/timetable"
+            secondary
+            className="flex items-center justify-center text-sm text-light-accent dark:text-dark-accent"
+          >
+            Download
+          </Link>
 
-        <div className="relative" ref={infoIconRef}>
-          <FiInfo
-            className="cursor-help opacity-40"
-            onClick={toggleInfoPopup}
-            onMouseEnter={toggleInfoPopup}
-            onMouseLeave={() => setShowInfoPopup(false)}
-          />
-          {showInfoPopup && (
-            <InfoPopup
-              bottom
-              text="Generate your full schedule timetable and download it as image."
-              onClose={() => setShowInfoPopup(false)}
+          <div className="relative" ref={infoIconRef}>
+            <FiInfo
+              className="cursor-help opacity-40"
+              onClick={toggleInfoPopup}
+              onMouseEnter={toggleInfoPopup}
+              onMouseLeave={() => setShowInfoPopup(false)}
             />
-          )}
+            {showInfoPopup && (
+              <InfoPopup
+                bottom
+                text="Generate your full schedule timetable and download it as image."
+                onClose={() => setShowInfoPopup(false)}
+              />
+            )}
+          </div>
         </div>
+        <Refresh type={{ mutateTimetable: true }} />
       </div>
+
       {timetable ? (
         <Container day={day} currentDayOrder={Number(currentDayOrder)} />
       ) : timetableLoading || dayLoading ? (
