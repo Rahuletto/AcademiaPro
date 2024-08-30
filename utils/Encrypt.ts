219a66efd9be13773b262219a22b20b473da1ba4
Rahuletto <rahulmarban@gmail.com>
2024-08-30 10:31:19 +0530

    feat: token validation

---
+++ utils/Encrypt.ts
@@ -0,0 +1,9 @@
+function createToken(secretKey: string): string {
+  const timestamp = Math.floor(Date.now() / 1000);
+  const data = `${timestamp}.${secretKey}`;
+
+  const encodedData = btoa(data); 
+  return encodedData;
+}
+
+export const token = createToken(process.env.NEXT_PUBLIC_VALIDATION_KEY || "");
