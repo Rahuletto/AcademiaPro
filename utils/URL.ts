61c81b1dc4d96af65ab00e5ff43ea66e641693b2
Rahuletto <rahulmarban@gmail.com>
2024-09-04 07:39:45 +0530

    fix: do url hoping

--- utils/URL.ts
+++ utils/URL.ts
@@ -1,1 +1,14 @@
-export const ProscrapeURL = process.env.NEXT_PUBLIC_API_URL;
+function getProscrape() {
+    const timePart = Math.floor(new Date().getHours() / 8) + 1;
+    switch (timePart) {
+        case 1:
+            return process.env.NEXT_PUBLIC_API_URL_MORN || process.env.NEXT_PUBLIC_API_URL;
+        case 2:
+            return process.env.NEXT_PUBLIC_API_URL_EVEN || process.env.NEXT_PUBLIC_API_URL;
+        case 3:
+            return process.env.NEXT_PUBLIC_API_URL_NIGHT || process.env.NEXT_PUBLIC_API_URL;
+        default:
+            throw new Error('Invalid time part calculation');
+    }
+}
+export const ProscrapeURL = getProscrape()
