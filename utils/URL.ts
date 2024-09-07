d0fbf37e638c6a02ad647952026179f62d8170f3
Rahul Marban <rahulmarban@gmail.com>
2024-09-07 09:28:27 +0530

    fix: making it random

--- utils/URL.ts
+++ utils/URL.ts
@@ -1,6 +1,6 @@
 function getProscrape() {
-    const timePart = getRand();
-    switch (timePart) {
+    const rand = Math.floor(Math.random() * 3) + 1;
+    switch (rand) {
         case 1:
             return process.env.NEXT_PUBLIC_API_URL_MORN || process.env.NEXT_PUBLIC_API_URL;
         case 2:
@@ -8,15 +8,9 @@ function getProscrape() {
         case 3:
             return process.env.NEXT_PUBLIC_API_URL_NIGHT || process.env.NEXT_PUBLIC_API_URL;
         default:
-            throw new Error('Invalid time part calculation');
+            return process.env.NEXT_PUBLIC_API_URL;
     }
 }
 
-function getRand(): number {
-    const currentHour = new Date().getHours();
-    const block = Math.floor(currentHour / 2);
-    const randomValue = (block + 1) * 73 % 3;
-    return randomValue + 1;
-}
 
 export const ProscrapeURL = getProscrape()
