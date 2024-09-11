2fc3f284bd89cf29e521ae636a9079624a434c0a
Srivishal Sivasubramanian <47695678+root-daemon@users.noreply.github.com>
2024-09-12 00:27:55 +0530

    feat: api

--- utils/URL.ts
+++ utils/URL.ts
@@ -1,15 +1,25 @@
 export function getUrl(key: string, endpoint: string) {
-    const rand = Math.floor(Math.random() * 3) + 1;
-    switch (rand) {
-        case 1:
-            return process.env.NEXT_PUBLIC_API_URL_MORN || process.env.NEXT_PUBLIC_API_URL;
-        case 2:
-            return process.env.NEXT_PUBLIC_API_URL_EVEN || process.env.NEXT_PUBLIC_API_URL;
-        case 3:
-            return process.env.NEXT_PUBLIC_API_URL_NIGHT || process.env.NEXT_PUBLIC_API_URL;
-        default:
-            return process.env.NEXT_PUBLIC_API_URL;
-    }
+  const rand = Math.floor(Math.random() * 4) + 1;
+  switch (rand) {
+    case 1:
+      return (
+        process.env.NEXT_PUBLIC_API_URL_MORN || process.env.NEXT_PUBLIC_API_URL
+      );
+    case 2:
+      return (
+        process.env.NEXT_PUBLIC_API_URL_EVEN || process.env.NEXT_PUBLIC_API_URL
+      );
+    case 3:
+      return (
+        process.env.NEXT_PUBLIC_API_URL_NIGHT || process.env.NEXT_PUBLIC_API_URL
+      );
+    case 4:
+      return (
+        process.env.NEXT_PUBLIC_API_URL_2 || process.env.NEXT_PUBLIC_API_URL
+      );
+    default:
+      return process.env.NEXT_PUBLIC_API_URL;
+  }
 }
 
 export const revalUrl = process.env.NEXT_PUBLIC_API_URL;
