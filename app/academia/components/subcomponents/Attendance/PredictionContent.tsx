191ab496b5faf18c1350bb14c96e1d196fcf989c
Srivishal Sivasubramanian <47695678+root-daemon@users.noreply.github.com>
2024-09-18 23:55:28 +0530

    fix: types

--- app/academia/components/subcomponents/Attendance/PredictionContent.tsx
+++ app/academia/components/subcomponents/Attendance/PredictionContent.tsx
@@ -1,11 +1,16 @@
-'use client'
+"use client";
 import React, { useEffect } from "react";
 import { useAttendancePrediction } from "@/hooks/useAttendancePredict";
 import Error from "@/components/States/Error";
 import Loading from "@/components/States/Loading";
 import NoData from "../NoData";
 import AttendanceList from "./Predict/AttendanceList";
-import { DateRange, AttendanceCourse, CalendarMonth, TimetableDay } from "@/types/Attendance";
+import {
+  DateRange,
+  AttendanceCourse,
+  CalendarMonth,
+  TimetableDay,
+} from "@/types/Attendance";
 import { useData } from "@/provider/DataProvider";
 import { usePlanner } from "@/provider/DataCalProvider";
 
@@ -13,18 +18,26 @@ interface PredictionContentProps {
   dateRange: DateRange;
 }
 
-export default function PredictionContent({ dateRange }: PredictionContentProps): JSX.Element {
-  const { calendar, isLoading: isLoadingCalendar, error: calendarError } = usePlanner();
-  const { timetable, attendance, isLoading: isLoadingTimetable, error: timetableError } = useData();
-
+export default function PredictionContent({
+  dateRange,
+}: PredictionContentProps): JSX.Element {
   const {
-    predictedAttendance,
-    performPrediction,
-  } = useAttendancePrediction(
+    calendar,
+    isLoading: isLoadingCalendar,
+    error: calendarError,
+  } = usePlanner();
+  const {
+    timetable,
+    attendance,
+    isLoading: isLoadingTimetable,
+    error: timetableError,
+  } = useData();
+
+  const { predictedAttendance, performPrediction } = useAttendancePrediction(
     attendance as AttendanceCourse[],
     timetable as TimetableDay[],
     calendar as CalendarMonth[],
-    dateRange
+    dateRange,
   );
 
   useEffect(() => {
@@ -34,8 +47,11 @@ export default function PredictionContent({ dateRange }: PredictionContentProps)
   }, [attendance, timetable, calendar, dateRange, performPrediction]);
 
   if (isLoadingCalendar || isLoadingTimetable) return <Loading size="max" />;
-  if (calendarError || timetableError) return <Error component="Attendance Prediction" />;
+  if (calendarError || timetableError)
+    return <Error error={calendarError || timetableError!} component="Attendance Prediction" />;
   if (!predictedAttendance) return <NoData component="Attendance Prediction" />;
 
-  return <AttendanceList open={true} displayedAttendance={predictedAttendance} />;
+  return (
+    <AttendanceList open={true} displayedAttendance={predictedAttendance} />
+  );
 }
