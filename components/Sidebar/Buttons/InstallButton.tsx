1100e2926887d54c2cbf9bca251204950eee552e
Rahuletto <rahulmarban@gmail.com>
2024-09-19 11:06:02 +0530

    feat: install button

--- components/Sidebar/Buttons/InstallButton.tsx
+++ components/Sidebar/Buttons/InstallButton.tsx
@@ -1,7 +1,6 @@
-'use client'
 import { GrInstallOption } from "react-icons/gr";
 import { usePwa } from "@dotmind/react-use-pwa";
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback } from "react";
 
 export default function InstallButton({
   ref,
@@ -10,41 +9,26 @@ export default function InstallButton({
   ref: any;
   anchor?: boolean;
 }) {
-  const deferredPrompt = useRef<any>(null)
+  const { installPrompt, isInstalled, isStandalone, isOffline, canInstall } =
+    usePwa();
 
-  useEffect(() => {
-    const handlePrompt = (e: any) => {
-      e.preventDefault()
-      deferredPrompt.current = e
+  const handleInstallPrompt = useCallback(() => {
+    if (canInstall) {
+      installPrompt();
     }
+  }, [canInstall, installPrompt]);
 
-    window.addEventListener('beforeinstallprompt', handlePrompt)
-    return () => window.removeEventListener('beforeinstallprompt', handlePrompt)
-  })
-
-  const installPWA = () => {
-    try {
-    if (!deferredPrompt.current) return
-    deferredPrompt.current.prompt()
-    deferredPrompt.current.userChoice.then((choiceResult: any) => {
-      if (choiceResult.outcome === 'accepted') {
-        deferredPrompt.current = null;
-      }
-    })
-  } catch {
-    alert("Seems like this isnt supported")
-  }
-  }
+  if (isOffline) return;
 
   return (
-    (
+    (!isInstalled || !isStandalone ) && canInstall && (
       <>
       <button
         ref={ref}
         type="button"
         name="Open navbar"
         className={`fixed bottom-[5.14rem] flex items-center justify-start gap-3 rounded-full p-3 transition-all duration-100 lg:bottom-36 ${anchor ? "right-[2.1rem] lg:left-8" : "lg:right-7"} bg-light-background-light dark:bg-dark-background-light`}
-        onClick={installPWA}
+        onClick={handleInstallPrompt}
       >
         <GrInstallOption
           className={`text-xl text-light-color transition-all duration-75 dark:text-dark-color`}
