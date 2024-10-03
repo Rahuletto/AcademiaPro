f411074a714302fc25de1d9f065298f7fe7fd0ad
Srivishal Sivasubramanian <47695678+root-daemon@users.noreply.github.com>
2024-10-03 20:38:24 +0530

    feat: maintenance message

--- app/maintenance/page.tsx
+++ app/maintenance/page.tsx
@@ -3,8 +3,8 @@ export default function Maintenance() {
     <main className="flex h-full flex-col items-center justify-center gap-4 bg-light-background-normal p-4 dark:bg-dark-background-normal">
       <div className="flex w-1/2 animate-fadeIn flex-col gap-6 self-center rounded-3xl bg-light-warn-background px-6 py-4 md:px-12 md:py-6 dark:bg-dark-warn-background">
         <h3 className="text-center text-light-warn-color dark:text-dark-warn-color">
-          Our servers are down due to overload. Sorry for the inconvenience, we
-          are working on it to get it back running again.
+          Academia has made a few changes, we will work on itegrating them,
+          Sorry for the inconvenience caused.
         </h3>
       </div>
       <h2 className="rounded-3xl bg-light-background-light px-6 py-4 text-center text-light-color dark:bg-dark-background-light dark:text-dark-color">
