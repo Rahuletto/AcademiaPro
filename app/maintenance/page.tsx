3b52e0bd9848a6b5a12849e3a4154d3f465a1ec1
Srivishal Sivasubramanian <47695678+root-daemon@users.noreply.github.com>
2024-10-03 20:44:48 +0530

    fix: spelling

--- app/maintenance/page.tsx
+++ app/maintenance/page.tsx
@@ -3,7 +3,7 @@ export default function Maintenance() {
     <main className="flex h-full flex-col items-center justify-center gap-4 bg-light-background-normal p-4 dark:bg-dark-background-normal">
       <div className="flex w-1/2 animate-fadeIn flex-col gap-6 self-center rounded-3xl bg-light-warn-background px-6 py-4 md:px-12 md:py-6 dark:bg-dark-warn-background">
         <h3 className="text-center text-light-warn-color dark:text-dark-warn-color">
-          Academia has made a few changes, we will work on itegrating them,
+          Academia has made a few changes, we will work on integrating them,
           Sorry for the inconvenience caused.
         </h3>
       </div>
