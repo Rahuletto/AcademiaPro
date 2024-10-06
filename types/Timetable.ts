b76a130967948de11557ea1482e9a26f896f75af
Rahuletto <rahulmarban@gmail.com>
2024-10-06 22:45:30 +0530

    fix: types

--- types/Timetable.ts
+++ types/Timetable.ts
@@ -4,6 +4,8 @@ export type TimeTableResponse = SuccessTimeTableResponse & ProscrapeError;
 export interface SuccessTimeTableResponse {
   regNumber: string;
   batch: string;
-  schedule: { day: number; table: (string | undefined)[] }[];
+  schedule: Table[];
   requestedAt: number | null;
 }
+
+export interface Table { day: number; table: (string | undefined)[] }
