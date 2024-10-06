f1201b6dd90f314f18aa90919ece8b049dc79b63
Srivishal Sivasubramanian <47695678+root-daemon@users.noreply.github.com>
2024-10-06 23:15:17 +0530

    feat: url

--- utils/URL.ts
+++ utils/URL.ts
@@ -1,9 +1,16 @@
-const serverUrls = ["https://newscrape.rameshrahul1974.workers.dev"];
+const serverUrls = [
+  process.env.NEXT_PUBLIC_API_URL_H,
+  process.env.NEXT_PUBLIC_API_URL_EVEN,
+  process.env.NEXT_PUBLIC_API_URL_NIGHT,
+  process.env.NEXT_PUBLIC_API_URL,
+  process.env.NEXT_PUBLIC_API_URL_MORN,
+  process.env.NEXT_PUBLIC_API_URL_2,
+];
 
 export const revalUrl = serverUrls[0];
 
 export default function rotateUrl(): string {
   const timestamp = Date.now();
   const index = timestamp % serverUrls.length;
-  return serverUrls[index];
+  return serverUrls[index] ?? "";
 }
