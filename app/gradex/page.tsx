d21486fd0b3120904b285640f155019ff44e8da6
root-daemon <srivishal.sivasubramanian@gmail.com>
2024-11-15 23:47:47 +0530

    feat: add gradex page

---
+++ app/gradex/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React, { useState } from "react";
+import { Link } from "next-view-transitions";
+import useSearch from "@/hooks/useSearch";
+import { Sidebar } from "@/components/Sidebar";
+
+import dynamic from "next/dynamic";
+
+export default function Urls() {
+  return (
+    <main className="h-screen w-full bg-light-background-normal pb-0 text-light-color dark:bg-dark-background-normal dark:text-dark-color">
+      <Sidebar>
+        <div className="flex flex-col gap-12">
+          <section id="links" className="flex flex-col gap-6">
+            <h1 className="text-2xl font-semibold">GradeX</h1>
+
+            <div className="ml-2 flex flex-col gap-20 md:ml-10"></div>
+          </section>
+        </div>
+      </Sidebar>
+    </main>
+  );
+}
