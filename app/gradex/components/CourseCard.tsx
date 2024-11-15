b2afbb8e51221afa1539d285ded966821b9876d2
root-daemon <srivishal.sivasubramanian@gmail.com>
2024-11-16 00:29:00 +0530

    feat: GradeX
    fix: corseName
    
    feat: it works
    
    feat: auto grade

---
+++ app/gradex/components/CourseCard.tsx
@@ -0,0 +1,85 @@
+import React, { useEffect } from "react";
+import { Slider } from "@/components/ui/slider";
+import { Mark } from "@/types/Marks";
+import { useData } from "@/provider/DataProvider";
+import { determineGrade } from "@/utils/Grade";
+
+const gradeMap: { [key: number]: string } = {
+  0: "C",
+  1: "B",
+  2: "B+",
+  3: "A",
+  4: "A+",
+  5: "O",
+};
+
+export default function MarkCard({
+  mark,
+  currentGrade,
+  updateGrade,
+}: {
+  mark: Mark;
+  currentGrade: string;
+  updateGrade: (courseCode: string, grade: string) => void;
+}) {
+  const { courses } = useData();
+  const courseDetails = courses?.find((a) => a.code === mark.courseCode);
+
+  useEffect(() => {
+    const lostMark: number =
+      Number(mark.overall.total) - Number(mark.overall.marks);
+    const calculatedGrade = determineGrade(lostMark);
+    updateGrade(mark.courseCode, calculatedGrade);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [mark.overall.total, mark.overall.marks, mark.courseCode]);
+
+  const getSliderValue = (grade: string) => {
+    return Object.entries(gradeMap).find(([_, g]) => g === grade)?.[0] || "5";
+  };
+
+  const handleSliderChange = (value: number[]) => {
+    const newGrade = gradeMap[value[0]];
+    updateGrade(mark.courseCode, newGrade);
+  };
+
+  return (
+    <div className="mb-4 space-y-2 rounded-lg bg-light-background-light p-4 text-light-color dark:bg-dark-background-light dark:text-dark-color">
+      <div className="font-semibold">
+        {!mark.courseName.toLowerCase().includes("n/a")
+          ? mark.courseName?.toLowerCase()
+          : courseDetails?.title || mark.courseCode}
+      </div>
+      <div className="text-sm text-light-accent dark:text-dark-accent">
+        Code: {mark.courseCode}
+      </div>
+      <div className="text-sm text-light-accent dark:text-dark-accent">
+        Type: {mark.courseType}
+      </div>
+      <div className="mt-2 grid grid-cols-3 items-center gap-4">
+        <div className="text-light-accent dark:text-dark-accent">
+          Credit: {courseDetails?.credit || 0}
+        </div>
+        <div>Grade: {currentGrade}</div>
+      </div>
+
+      <div className="flex items-center justify-between text-xs text-light-accent dark:text-dark-accent">
+        <span>C</span>
+        <span>B</span>
+        <span>B+</span>
+        <span>A</span>
+        <span>A+</span>
+        <span>O</span>
+      </div>
+      <Slider
+        max={5}
+        step={1}
+        value={[parseInt(getSliderValue(currentGrade))]}
+        onValueChange={handleSliderChange}
+        className="w-full"
+      />
+      <div className="mt-2 text-sm">
+        Overall: {mark.overall.marks}/{mark.overall.total}
+      </div>
+    </div>
+  );
+}
--- app/gradex/page.tsx
+++ app/gradex/page.tsx
@@ -1,16 +1,101 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Sidebar } from "@/components/Sidebar";
+import { useData } from "@/provider/DataProvider";
+import Loading from "@/components/States/Loading";
+import Error from "@/components/States/Error";
+import MarkCard from "./components/CourseCard";
 
-export default function Urls() {
+export default function GradeX() {
+  const { marks, isLoading, error, courses, isValidating } = useData();
+  const [grades, setGrades] = useState<{ [courseCode: string]: string }>({});
+  const [sgpa, setSgpa] = useState(0);
+
+  const gradePoints: { [key: string]: number } = {
+    O: 10,
+    "A+": 9,
+    A: 8,
+    "B+": 7,
+    B: 6,
+    C: 5,
+  };
+
+  useEffect(() => {
+    if (marks) {
+      const initialGrades = Object.fromEntries(
+        marks.map((mark) => [mark.courseCode, "O"]),
+      );
+      setGrades(initialGrades);
+    }
+  }, [marks]);
+
+  useEffect(() => {
+    sgpaCalculator();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [grades, courses]);
+
+  const updateGrade = (courseCode: string, grade: string) => {
+    setGrades((prevGrades) => ({
+      ...prevGrades,
+      [courseCode]: grade,
+    }));
+  };
+
+  const sgpaCalculator = () => {
+    if (!courses || !grades) return;
+
+    let totalPoints = 0;
+    let totalCredits = 0;
+
+    Object.entries(grades).forEach(([courseCode, grade]) => {
+      const course = courses.find((c) => c.code === courseCode);
+      if (course) {
+        const credits = Number(course.credit);
+        const gradePoint = gradePoints[grade] || 0;
+
+        totalPoints += credits * gradePoint;
+        totalCredits += credits;
+      }
+    });
+
+    const calculatedSgpa = totalCredits > 0 ? totalPoints / totalCredits : 0;
+    setSgpa(parseFloat(calculatedSgpa.toFixed(2)));
+  };
+
   return (
     <main className="h-screen w-full bg-light-background-normal pb-0 text-light-color dark:bg-dark-background-normal dark:text-dark-color">
       <Sidebar>
         <div className="flex flex-col gap-12">
           <section id="links" className="flex flex-col gap-6">
-            <h1 className="text-2xl font-semibold">GradeX</h1>
-
-            <div className="ml-2 flex flex-col gap-20 md:ml-10"></div>
+            <h1 className="text-3xl font-semibold">GradeX</h1>
+            <div className="bg-light-background-dark p-4 px-5 pb-5 dark:bg-dark-background-dark">
+              <h2 className="text-2xl font-semibold capitalize">
+                SGPA Calculator: {sgpa}
+              </h2>
+              <div>
+                {isLoading || isValidating ? (
+                  <Loading size="3xl" />
+                ) : error ? (
+                  <Error component="GradeX" error={error} />
+                ) : marks ? (
+                  <div className="grid animate-fadeIn grid-cols-marks gap-2 transition-all duration-200">
+                    {marks
+                      .filter((a) => a.courseType === "Theory")
+                      .map((mark, index) => (
+                        <div className="py-4" key={mark.courseCode}>
+                          <MarkCard
+                            mark={mark}
+                            currentGrade={grades[mark.courseCode] || "O"}
+                            updateGrade={updateGrade}
+                          />
+                        </div>
+                      ))}
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </div>
+            </div>
           </section>
         </div>
       </Sidebar>
