c95096ecf0e06f75311a92a5933e706c638cfbc2
Rahuletto <rahulmarban@gmail.com>
2024-11-19 12:08:18 +0530

    style: new slider ui

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as SliderPrimitive from "@radix-ui/react-slider"
+import * as React from "react";
+import * as SliderPrimitive from "@radix-ui/react-slider";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
@@ -13,16 +13,28 @@ const Slider = React.forwardRef<
     ref={ref}
     className={cn(
       "relative flex w-full touch-none select-none items-center",
-      className
+      className,
     )}
     {...props}
   >
     <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-neutral-900/20 dark:bg-neutral-50/20">
-      <SliderPrimitive.Range className="absolute h-full rounded-full pr-2 bg-neutral-900 dark:bg-neutral-50" />
+      <SliderPrimitive.Range className="absolute h-full rounded-full bg-neutral-900 pr-2 dark:bg-neutral-50" />
+      <div className="absolute top-0.5 flex w-full justify-between">
+        {[...Array(6)].map((_, index) =>
+          index === 0 || index === 5 ? (
+            <div key={index} className="w-2" />
+          ) : (
+            <div
+              key={index}
+              className="h-1 w-1 rounded-full bg-light-background-light dark:bg-dark-background-light"
+            />
+          ),
+        )}
+      </div>
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block border border-neutral-400 h-4 w-4 bg-neutral-900 dark:bg-neutral-50 rounded-full transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-800 dark:border-neutral-50/50 dark:focus-visible:ring-neutral-300" />
+    <SliderPrimitive.Thumb className="z-20 block h-6 w-2 active:w-1 rounded-full bg-neutral-900 dark:bg-neutral-50 outline outline-4 outline-light-background-normal dark:outline-dark-background-normal focus-visible:ring-2 focus-visible:ring-light-background-normal dark:focus-visible:ring-dark-background-normal transition-all duration-200 disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
-))
-Slider.displayName = SliderPrimitive.Root.displayName
+));
+Slider.displayName = SliderPrimitive.Root.displayName;
 
-export { Slider }
+export { Slider };
