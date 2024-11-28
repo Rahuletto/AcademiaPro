7f1863afc3832b9785c2fe4f3c446c8bde7e9ef2
Rahuletto <rahulmarban@gmail.com>
2024-11-28 18:57:31 +0530

    chore: changing determineGrade for some accurate stuff

--- utils/Grade.ts
+++ utils/Grade.ts
@@ -1,8 +1,8 @@
 export function determineGrade(lostMark: number): string {
-  if (lostMark >= 40) return "B";
-  if (lostMark >= 30) return "B+";
-  if (lostMark >= 20) return "A";
-  if (lostMark >= 10) return "A+";
+  if (lostMark >= 36) return "B";
+  if (lostMark >= 27) return "B+";
+  if (lostMark >= 18) return "A";
+  if (lostMark >= 9) return "A+";
   if (lostMark >= 0) return "O";
   return "C";
 }
