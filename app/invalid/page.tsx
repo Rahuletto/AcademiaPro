df90188505fa67ab7b1d80c72df6e017a80abd88
Rahuletto <rahulmarban@gmail.com>
2025-01-18 18:19:33 +0530

    feat: misc pages

---
+++ app/invalid/page.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'next-view-transitions'
+import React from 'react'
+import { FaQuestion } from 'react-icons/fa'
+import { VscSquirrel } from 'react-icons/vsc'
+
+export default function invalid() {
+  return (
+    <main className='h-screen w-screen bg-light-background-normal flex-col dark:bg-dark-background-normal flex items-center justify-center'>
+        <div className='flex w-fit flex-col items-center justify-center relative'>
+        <VscSquirrel className='text-8xl text-light-accent dark:text-dark-accent' />
+        <FaQuestion className='text-4xl text-light-accent dark:text-dark-accent absolute top-0 right-0 -mr-9 rotate-12 opacity-60' />
+        </div>
+        <div className='flex items-center flex-col mt-4 gap-2 justify-center text-light-color dark:text-dark-color'>
+            <h1 className='text-2xl font-semibold'>Session expired/invalid</h1>
+            <p className='font-mono opacity-70 max-w-screen-md text-center mx-3'>Our squirrels (delulu) found out you have an invalid or expired session token, Because SRM didn&apos;t return us the page with your token, we want you to reset and login again</p>
+
+            <Link href='/auth/logout' className='bg-light-accent mt-12 dark:bg-dark-accent text-light-background-normal dark:text-dark-background-normal p-2 px-5 rounded-xl font-semibold hover:bg-light-button-hover dark:hover:bg-dark-button-hover'>Reset</Link>
+        </div>
+    </main>
+  )
+}
---
+++ components/Error.tsx
@@ -0,0 +1,42 @@
+"use client"; // Error components must be Client Components
+
+import { Link } from "next-view-transitions";
+import { useEffect } from "react";
+import { BiError } from "react-icons/bi";
+
+export default function Error({ error }: { error: Error | string }) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="h-screen p-4">
+      <div className="h-full w-full rounded-2xl bg-dark-background-dark p-2">
+        <div className="flex h-full animate-fadeIn flex-col items-center justify-center gap-2 rounded-xl bg-light-error-background p-8 text-light-error-color dark:bg-dark-error-background dark:text-dark-error-color">
+          <BiError className="mb-3 text-4xl" />
+          <h1 className="text-2xl font-medium text-light-error-color dark:text-dark-error-color">
+            Error.
+          </h1>
+          <p className="text-md max-w-[500px] text-center italic text-light-error-color opacity-90 dark:text-dark-error-color">
+            *intense crash sound*
+          </p>
+
+          <pre className="lg:text-md mx-2 mt-4 max-h-[500px] w-[80vw] overflow-auto rounded-2xl border-2 border-dashed border-light-error-color p-3 text-xs text-light-error-color opacity-90 md:text-sm dark:border-dark-error-color dark:text-dark-error-color">
+            <code>{typeof error === "string" ? error : error.stack}</code>
+          </pre>
+        </div>
+      </div>
+      <div className="absolute bottom-12 left-0 flex w-screen flex-wrap items-center justify-center gap-3 text-light-error-color opacity-90 dark:text-dark-error-color">
+        <span className="text-sm opacity-70">
+          Frequent errors? Try resetting (It logs you out tho)
+        </span>
+        <Link
+          href="/auth/logout"
+          className="rounded-full bg-light-error-color px-3 py-0.5 font-semibold text-light-error-background dark:bg-dark-error-color dark:text-dark-error-background"
+        >
+          Clear cookies
+        </Link>
+      </div>
+    </div>
+  );
+}
