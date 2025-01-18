f45ac8dc8799fd8afca18c5395ab8485eb659058
Rahuletto <rahulmarban@gmail.com>
2025-01-18 13:53:54 +0530

    chore: configs

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  poweredByHeader: false,
+  compress: true,
+  
+  experimental: {
+    // reactCompiler: true,
+    parallelServerCompiles: true,
+    nextScriptWorkers: true,
+    useLightningcss: true,
+    turbo: {
+      treeShaking: true,
+
+    }
+  }
+};
+export default nextConfig
