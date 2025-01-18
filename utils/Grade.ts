333e9dc9fc155331319fbcfc6efc2236ade6f285
Rahuletto <rahulmarban@gmail.com>
2025-01-18 17:35:02 +0530

    todo: gradex later

--- utils/Grade.ts
+++
@@ -1,8 +0,0 @@
-export function determineGrade(lostMark: number): string {
-  if (lostMark >= 36) return "B";
-  if (lostMark >= 27) return "B+";
-  if (lostMark >= 18) return "A";
-  if (lostMark >= 9) return "A+";
-  if (lostMark >= 0) return "O";
-  return "C";
-}
