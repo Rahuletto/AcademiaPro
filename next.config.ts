7c0e3589915e884f9dd746ad566bcc6134cd9c31
Rahuletto <rahulmarban@gmail.com>
2025-01-18 21:48:46 +0530

    chore: configs and biome setup

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  poweredByHeader: false,
-  compress: true,
-  
-  experimental: {
-    // reactCompiler: true,
-    parallelServerCompiles: true,
-    nextScriptWorkers: true,
-    useLightningcss: true,
-    turbo: {
-      treeShaking: true,
+	poweredByHeader: false,
+	compress: true,
 
-    }
-  }
+	experimental: {
+		// reactCompiler: true,
+		parallelServerCompiles: true,
+		nextScriptWorkers: true,
+		useLightningcss: true,
+		turbo: {
+			treeShaking: true,
+		},
+	},
 };
-export default nextConfig
+export default nextConfig;
