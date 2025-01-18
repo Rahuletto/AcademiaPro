d3f13309c515b3d7caa97e7dce3cecf45dfbb921
Rahuletto <rahulmarban@gmail.com>
2025-01-18 23:17:11 +0530

    feat: academia page

---
+++ app/academia/layout.tsx
@@ -0,0 +1,58 @@
+import { Sidebar } from "@/components/Sidebar";
+import DayOrder from "@/components/Sidebar/Badges/DayOrder";
+import ProfileBadge from "@/components/Sidebar/ProfileBadge";
+import { fetchUserData } from "@/hooks/fetchUserData";
+import type { UserInfo } from "@/types/User";
+import { Link } from "next-view-transitions";
+import type { ReactNode } from "react";
+import { BiLogInCircle } from "react-icons/bi";
+
+export default async function RootLayout({
+	children,
+}: Readonly<{
+	children: ReactNode;
+}>) {
+	const json = await fetchUserData();
+
+	return (
+		<div className="h-screen flex-shrink-0 w-full flex flex-row bg-light-background-normal lg:p-2 transition-all duration-150 text-light-color dark:bg-dark-background-normal dark:text-dark-color">
+			<div id="attendance-predict" className="z-30" />
+			<Sidebar
+				dayorder={<DayOrder />}
+				mini={
+					<DayOrder
+						mini
+						className="fixed hidden right-9 md:flex items-center justify-center"
+					/>
+				}
+				profile={
+					json?.error ? (
+						<Link
+							href="/auth/login"
+							className="text-light-success-color dark:text-dark-success-color bg-light-success-background dark:bg-dark-success-background rounded-full font-semibold flex items-center justify-center py-3 lg:w-[82%] gap-2 "
+						>
+							<BiLogInCircle className="text-xl" /> Login
+						</Link>
+					) : (
+						<ProfileBadge user={json?.user as UserInfo} />
+					)
+				}
+			/>
+			<div
+				id="content"
+				className={
+					"fixed h-full w-full flex-1 transform duration-150 md:relative md:w-full"
+				}
+				style={{ marginLeft: "var(--sidebar-width)" }}
+			>
+				<main
+					className={
+						"m-0.5 p-5 h-full w-full overflow-y-auto overflow-x-hidden scroll-smooth rounded-2xl bg-light-background-light transition-all duration-150  dark:bg-dark-background-dark"
+					}
+				>
+					<div style={{ height: "-webkit-fill-available" }}>{children}</div>
+				</main>
+			</div>
+		</div>
+	);
+}
---
+++ app/academia/page.tsx
@@ -0,0 +1,53 @@
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
+import Loading from "@/components/States/Loading";
+import { fetchUserData } from "@/hooks/fetchUserData";
+import { supabase } from "@/utils/Database/supabase";
+import { cookies } from "next/headers";
+import { encode } from "@/utils/Cookies";
+
+const Attendance = dynamic(() => import("./components/Attendance"), {
+	ssr: true,
+});
+
+const Marks = dynamic(() => import("./components/Marks"), { ssr: true });
+const Timetable = dynamic(() => import("./components/Timetable"), {
+	ssr: true,
+});
+
+export default async function Academia() {
+	const json = await fetchUserData();
+	const cookie = (await cookies()).get("key");
+	let ophours = [];
+
+	const { data, error } = await supabase
+		.from("goscrape")
+		.select("ophour")
+		.eq("token", encode(cookie?.value ?? ""))
+		.single();
+
+	if (error) {
+		console.error("Error fetching ophours:", error);
+	} else {
+		ophours = data?.ophour?.split(",");
+	}
+
+	return (
+		<>
+			<div id="items" className="flex flex-col gap-12 relative">
+				<Suspense fallback={<Loading size="xl" />}>
+					<Timetable
+						ophours={ophours ?? []}
+						schedule={json.timetable?.schedule}
+					/>
+				</Suspense>
+				<Suspense fallback={<Loading size="xl" />}>
+					<Attendance data={json} />
+				</Suspense>
+				<Suspense fallback={<Loading size="xl" />}>
+					<Marks marks={json.marks?.marks} courses={json.courses?.courses} />
+				</Suspense>
+			</div>
+		</>
+	);
+}
