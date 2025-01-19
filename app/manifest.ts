03d39395151c1bdcd2ed4db22bafc96b73c3594b
Rahuletto <rahulmarban@gmail.com>
2025-01-19 19:33:56 +0530

    fix: maskable icons

--- app/manifest.ts
+++ app/manifest.ts
@@ -27,13 +27,13 @@ export default function manifest(): MetadataRoute.Manifest {
 		],
 		icons: [
 			{
-				src: "/icons/web-app-manifest-192x192.png",
+				src: "/icons/maskable_icon_x192.png",
 				sizes: "192x192",
 				type: "image/png",
 				purpose: "maskable",
 			},
 			{
-				src: "/icons/web-app-manifest-512x512.png",
+				src: "/icons/maskable_icon_x512.png",
 				sizes: "512x512",
 				type: "image/png",
 				purpose: "maskable",
