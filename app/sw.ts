de8f049d99f1b154e97b3f23c56258a42051a73f
Rahuletto <rahulmarban@gmail.com>
2025-01-20 09:55:08 +0530

    fix: fixing serwist

--- app/sw.ts
+++ app/sw.ts
@@ -1,16 +1,5 @@
-import { defaultCache } from "@serwist/next/worker";
-import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
 import { Serwist } from "serwist";
 
-// This declares the value of `injectionPoint` to TypeScript.
-// `injectionPoint` is the string that will be replaced by the
-// actual precache manifest. By default, this string is set to
-// `"self.__SW_MANIFEST"`.
-declare global {
-	interface WorkerGlobalScope extends SerwistGlobalConfig {
-		__SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
-	}
-}
 
 declare const self: ServiceWorkerGlobalScope;
 
