a582c1b16bd12616f6d578daa4bc39b55c533270
Rahuletto <rahulmarban@gmail.com>
2025-01-28 16:44:15 +0530

    feat: new server instances

--- utils/URL.ts
+++ utils/URL.ts
@@ -1,4 +1,4 @@
-const serverUrls = [process.env.NEXT_PUBLIC_URL, process.env.NEXT_PUBLIC_URL_GOAT, process.env.NEXT_PUBLIC_URL_ONE];
+const serverUrls = [process.env.NEXT_PUBLIC_URL, process.env.NEXT_PUBLIC_URL_GOAT, process.env.NEXT_PUBLIC_URL_ONE, process.env.NEXT_PUBLIC_URL_SERVER, process.env.NEXT_PUBLIC_URL_SRM];
 
 export const revalUrl = serverUrls[0];
 export const filesUrl = process.env.NEXT_PUBLIC_FILES_URL;
